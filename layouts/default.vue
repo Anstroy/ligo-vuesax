35d4720efcfda06e6ec016048ad4d2a22eb1022a
Aus Gomez <austrebertog@gmail.com>
2020-09-18 18:45:44 -0500

    repo init

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <sidebar />
+    <div class="container flex items-center center">
+      <h1>you see me?</h1>
+      <Nuxt />
+    </div>
+  </div>
+</template>
+
+<script>
+import sidebar from '~/components/sidebar'
+export default {
+  components: {
+    sidebar,
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">Soy del index</div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuesaxLogo from '~/components/VuesaxLogo.vue'
+
+export default {
+  components: {
+    Logo,
+    VuesaxLogo,
+  },
+}
+</script>
